f466edbf58e756c97faa72ee3d7da0a484e84b43
sandyrodriguesjs <122131843+sandyrodriguesjs@users.noreply.github.com>
2023-06-18 11:19:53 -0300

    conversões

---
+++ curso_java/casting.java
@@ -0,0 +1,44 @@
+package curso_java;
+
+public class casting {
+
+	public static void main(String[] args) {
+		
+		/*int x = 5;
+		int y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		*/
+		
+		/*int x;
+		double y;
+		
+		x = 5;
+		y = 2 * 5;
+		
+		System.out.println(x);
+		System.out.println(y);*/
+		
+		/*int b = 6;
+		int B = 8;
+		int h = 5;
+		
+		int area = ((b + B)/2) * h;
+		
+		System.out.println(area);*/
+		
+		/*int a = 5;
+		int b = 2;
+		
+		double resultado = (double) a/b;
+		
+		System.out.println(resultado);*/
+		
+		double a = 5.0;
+		int b = (int) a;
+		
+		System.out.println(b);
+	}
+
+}
